1313cfed0c2adea7cf7d2710372a3ca9c7b1aa24
Doha0 <abu.jardoha@gmail.com>
2023-06-05 18:16:41 +0600

    delete from menu added

--- index.js
+++ index.js
@@ -125,6 +125,20 @@ async function run() {
             const result = await menuCollection.find().toArray();
             res.send(result);
         })
+
+        app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await menuCollection.deleteOne(query);
+            res.send(result);
+          })
+
+        app.post('/menu', async (req, res) => {
+            const newItem = req.body;
+            const result = await menuCollection.insertOne(newItem);
+            res.send(result);
+        })
+
         // review collection
         app.get('/reviews', async (req, res) => {
             const result = await reviewCollection.find().toArray();
